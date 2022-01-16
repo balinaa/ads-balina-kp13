0bb035965cf5273ac76ca8a36890302f711512c8
alina <balina@dlit.dp.ua>
2022-01-16 17:55:30 +0200

    Create Program.cs

---
+++ lab1/lab1.ex2.asd/lab1.ex2.asd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab1.ex2.asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write n: ");
+            int n = int.Parse(Console.ReadLine());
+            int k, y;
+            for (int i = 1; i <= n; i++)
+            {
+                y = i;
+                k = 1;
+                while (y != 0)
+                {
+                    y = y / 10;
+                    k = k * 10;
+                }
+                if (i * (i - 1) % k == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
