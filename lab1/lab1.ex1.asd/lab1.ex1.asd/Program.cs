e4339885e511cc40ac421ce3ba9af8d0c1d44ea2
alina <balina@dlit.dp.ua>
2022-01-16 17:54:53 +0200

    Create Program.cs

---
+++ lab1/lab1.ex1.asd/lab1.ex1.asd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1.ex1.asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write x: ");
+            double x = int.Parse(Console.ReadLine());
+            Console.WriteLine("write y: ");
+            double y = int.Parse(Console.ReadLine());
+            Console.WriteLine("write z: ");
+            double z = int.Parse(Console.ReadLine());
+
+            double a = (y-Math.Sqrt(Math.Abs(Math.Pow(x,3))))/ (Math.Sqrt(Math.Pow(x,3)+5*Math.Pow(y,(-z))+Math.Pow(z,2)));
+            double b = Math.Sin(Math.Pow(a, (-x))) + y;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
