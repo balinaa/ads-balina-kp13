dfb222d3df2232a2b6ba506c770efe6ab571cac2
alina <balina@dlit.dp.ua>
2022-06-27 22:00:40 +0300

    Create Entry.cs

---
+++ lab 7/Entry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class Entry
+    {
+        public Entry(Key key, Value value)
+        {
+            Key = key;
+            Value = value;
+        }
+        public Entry()
+        {
+
+        }
+
+        public Key Key{ get; set; }
+        public Value Value { get; set; }
+    }
+}
