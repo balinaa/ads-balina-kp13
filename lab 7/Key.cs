51a657f51dc44e307f4306899f47fcf76249a093
alina <balina@dlit.dp.ua>
2022-06-27 22:00:45 +0300

    Create Key.cs

---
+++ lab 7/Key.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class Key
+    {
+        public Key(string flightCode)
+        {
+            FlightCode = flightCode;
+        }
+        public Key()
+        {
+            FlightCode = string.Empty;
+        }
+        public string FlightCode { get; set; }
+
+        public static bool operator== (Key key1, Key key2)
+        {
+            return key1.FlightCode == key2.FlightCode;
+        }
+        public static bool operator !=(Key key1, Key key2)
+        {
+            return key1.FlightCode != key2.FlightCode;
+        }
+
+        public override string ToString()
+        {
+            return FlightCode;
+        }
+    }
+}
